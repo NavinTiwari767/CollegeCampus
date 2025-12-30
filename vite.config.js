812e81e8c27c09cbe4f8ad15145426960dad7ad0
NavinTiwari767 <navin.tiwari.cse.2022@nist.edu>
2025-12-30 11:42:09 +0530

    force static sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        { src: "public/sitemap.xml", dest: "." },
-        { src: "public/robots.txt", dest: "." }
-      ]
-    })
-  ]
-});
+  plugins: [react()],
+})
